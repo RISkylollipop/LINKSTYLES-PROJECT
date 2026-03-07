f12e60343226fe35c3dfcdcc00bff20ca176ecd9
RISkylollipop <kelanikdas1@gmail.com>
2026-03-07 17:47:06 +0100

    Adjust the database connection

--- database.js
+++ database.js
@@ -1,14 +1,15 @@
 const mysql = require(`mysql2`)
 require(`dotenv`).config()
 
-const db = mysql.createConnection({ 
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DATABASE,
-    port: process.env.MYSQLPORT
-})
+// const db = mysql.createConnection({ 
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DATABASE,
+//     port: process.env.MYSQLPORT
+// })
 
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL)
 
 db.connect((err, result) => {
 
